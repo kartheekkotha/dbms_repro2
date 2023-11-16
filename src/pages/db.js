ff88cc60c64d884841da6e5b8472577c6f686811
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-11-16 22:54:38 +0530

    Delete unused components and update styles

--- src/pages/db.js
+++ src/pages/db.js
@@ -1,23 +1,23 @@
 const mysql = require('mysql2');
 
-// const db = mysql.createConnection({
-//   host:  "sql12.freesqldatabase.com",
-//   port: 3306,
-//   user: 'sql12660213',
-//   password: 'KjsLxnMLwY',
-//   database: 'sql12660213',
-// });
 const db = mysql.createConnection({
-  host: '127.0.0.1',
+  host: "sql12.freesqldatabase.com",
   port: 3306,
-  user: 'root',
-  password: 'Kartheek@123',
-  database: 'repro',
+  user: 'sql12660213',
+  password: 'KjsLxnMLwY',
+  database: 'sql12660213',
 });
+// const db = mysql.createConnection({
+//   host: '127.0.0.1',
+//   port: 3306,
+//   user: 'root',
+//   password: 'Kartheek@123',
+//   database: 'repro',
+// });
 
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error('Error connecting to MySQL:',err);
   } else {
     console.log('Connected to MySQL database');
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,33 @@
 // pages/index.js
 import React from 'react';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
-import TitleBox from '../components/TitleBox';
-import StartPage from '../pages/Start';
-import ProfProfilePage from '../pages/ProfProfile';
 
 const Home = () => {
+  const backgroundImageUrl = '/bg.jpg'; // Replace with your image URL
 
   return (
     <div className={styles.container}>
-      <TitleBox/>  
-      <StartPage/>
+      <div className={styles.background} style={{ backgroundImage: `url(${backgroundImageUrl})` }}></div>
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <img src="/repro_logo.png" alt="Repro Logo" className={styles.logo} />
+        </div>
+        <div className={styles.iconContainer}>
+          <Link href="/StudentSignUpPage">
+            <button className={styles.button}>Student Sign Up</button>
+          </Link>
+          <Link href="/ProfessorSignUpPage">
+            <button className={styles.button}>Professor Sign Up</button>
+          </Link>
+          <Link href="/StudentLoginPage">
+            <button className={styles.button}>Student Login</button>
+          </Link>
+          <Link href="/ProfessorLoginPage">
+            <button className={styles.button}>Professor Login</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
